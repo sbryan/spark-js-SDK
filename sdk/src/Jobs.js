f9a1e35ed8c6d81d3382a41ec23be10323c04c68
Steven Watanabe <steven.watanabe@autodesk.com>
2015-04-21 16:16:59 -0400

    Starting to fill out some jobs stuff

--- sdk/src/Jobs.js
+++ sdk/src/Jobs.js
@@ -15,15 +15,60 @@ var ADSKSpark = ADSKSpark || {};
     ADSKSpark.Jobs.prototype = Object.create(ADSKSpark.Paginated.prototype);
     ADSKSpark.Jobs.prototype.constructor = ADSKSpark.Jobs;
 
+    /**
+     * Get jobs registered to a member.
+     * @param {Object} params - limit/offset/sort/filter options.
+     * @returns {Promise} - A promise that will resolve to an array of jobs.
+     */
+    ADSKSpark.Jobs.get = function (params) {
+        return Client.authorizedApiRequest('/print/jobs')
+            .get(params)
+            .then(function (data) {
+                return new ADSKSpark.Jobs(data);
+            });
+    };
+
     ADSKSpark.Jobs.prototype._parse = function (data) {
         ADSKSpark.Paginated.prototype._parse.apply(this, data);
 
-        var jobs = data.printer_jobs;
+        var jobs = data.jobs || data.printer_jobs;
         if (Array.isArray(jobs)) {
             var that = this;
             jobs.forEach(function (job) {
-                that.push(job);
+                if (!job.printer_id) {
+                    job.printer_id = data.printer_id;
+                }
+                that.push(new ADSKSpark.Job(job));
             });
+        }
+    };
+
+    ADSKSpark.Job = function (data) {
+        this.id = data.job_id;
+        this.printer_id = data.printer_id;
+        this.raw = data;
+        this.status = null;
+    };
+
+    ADSKSpark.Job.prototype = {
+
+        constructor: ADSKSpark.Job,
+
+        /**
+         * Get the status of a print job.
+         * @returns {Promise} - A Promise that will resolve to the status information.
+         */
+        getStatus: function () {
+            var that = this;
+            return Client.authorizedApiRequest('/print/jobs/' + this.id)
+                .get()
+                .then(function (data) {
+                    that.status = data;
+                    return data;
+                })
+                .catch(function (error) {
+                    that.status = null;
+                });
         }
     };
 
