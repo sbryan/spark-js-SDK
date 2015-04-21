8c1b70407ab686f6b43d1f5af600261fc9e61554
Steven Watanabe <steven.watanabe@autodesk.com>
2015-04-21 13:20:24 -0400

    Comment out some code causing failing tests

--- sdk/src/Jobs.js
+++ sdk/src/Jobs.js
@@ -2,29 +2,29 @@ var ADSKSpark = ADSKSpark || {};
 
 (function () {
     var Client = ADSKSpark.Client;
-
-    /**
-     * A paginated array of jobs.
-     * @param {Object} data - JSON data.
-     * @constructor
-     */
-    ADSKSpark.Jobs = function (data) {
-        ADSKSpark.Paginated.call(this, data);
-    };
-
-    ADSKSpark.Jobs.prototype = Object.create(ADSKSpark.Paginated);
-    ADSKSpark.Jobs.prototype.constructor = ADSKSpark.Jobs;
-
-    ADSKSpark.Jobs.prototype._parse = function (data) {
-        ADSKSpark.Paginated.prototype._parse.apply(this, data);
-
-        var jobs = data.printer_jobs;
-        if (Array.isArray(jobs)) {
-            var that = this;
-            jobs.forEach(function (job) {
-                that.push(job);
-            });
-        }
-    };
+//
+//    /**
+//     * A paginated array of jobs.
+//     * @param {Object} data - JSON data.
+//     * @constructor
+//     */
+//    ADSKSpark.Jobs = function (data) {
+//        ADSKSpark.Paginated.call(this, data);
+//    };
+//
+//    ADSKSpark.Jobs.prototype = Object.create(ADSKSpark.Paginated);
+//    ADSKSpark.Jobs.prototype.constructor = ADSKSpark.Jobs;
+//
+//    ADSKSpark.Jobs.prototype._parse = function (data) {
+//        ADSKSpark.Paginated.prototype._parse.apply(this, data);
+//
+//        var jobs = data.printer_jobs;
+//        if (Array.isArray(jobs)) {
+//            var that = this;
+//            jobs.forEach(function (job) {
+//                that.push(job);
+//            });
+//        }
+//    };
 
 })();
