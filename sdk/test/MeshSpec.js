a496cdc912cb50195af5bdda6063f04974cb9c9b
Paul Breslin <paul.breslin@autodesk.com>
2015-04-22 10:05:35 -0400

    Experiment using local PM server.

--- sdk/test/MeshSpec.js
+++ sdk/test/MeshSpec.js
@@ -8,6 +8,8 @@ describe('Mesh API tests', function() {
     });
 
     beforeEach(function() {
+        var testApiUrl = 'http://localhost:9998';
+        ADSKSpark.Client.initialize(null, null, null, testApiUrl);
     });
 
     afterEach(function() {
