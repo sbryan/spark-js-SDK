9c34e99e81e86dc5cb9ce1c32d275cf1c7827ac6
Steven Watanabe <steven.watanabe@autodesk.com>
2015-04-22 16:26:55 -0400

    Remove needsFirmwareUpgrade() and related code from Printer
    Sadly we don't have any way to find out the latest firmware version for a particular printer type
    Would also be nice if there was a standard way to find firmware urls

--- sdk/src/Printer.js
+++ sdk/src/Printer.js
@@ -85,54 +85,6 @@ var ADSKSpark = ADSKSpark || {};
             });
     };
 
-    // http://stackoverflow.com/questions/6832596/how-to-compare-software-version-number-using-js-only-number
-    function versionCompare(v1, v2, options) {
-        var lexicographical = options && options.lexicographical,
-            zeroExtend = options && options.zeroExtend,
-            v1parts = v1.split('.'),
-            v2parts = v2.split('.');
-
-        function isValidPart(x) {
-            return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
-        }
-
-        if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
-            return NaN;
-        }
-
-        if (zeroExtend) {
-            while (v1parts.length < v2parts.length) v1parts.push("0");
-            while (v2parts.length < v1parts.length) v2parts.push("0");
-        }
-
-        if (!lexicographical) {
-            v1parts = v1parts.map(Number);
-            v2parts = v2parts.map(Number);
-        }
-
-        for (var i = 0; i < v1parts.length; ++i) {
-            if (v2parts.length == i) {
-                return 1;
-            }
-
-            if (v1parts[i] == v2parts[i]) {
-                continue;
-            }
-            else if (v1parts[i] > v2parts[i]) {
-                return 1;
-            }
-            else {
-                return -1;
-            }
-        }
-
-        if (v1parts.length != v2parts.length) {
-            return -1;
-        }
-
-        return 0;
-    }
-
     ADSKSpark.Printer.prototype = {
 
         constructor: ADSKSpark.Printer,
@@ -217,26 +169,6 @@ var ADSKSpark = ADSKSpark || {};
          */
         calibrate: function () {
             return this.sendCommand('calibrate');
-        },
-
-        _kLatestFirmwareVersion: "1.1.20150219.0", // TODO: ember only. add to config?
-
-        /**
-         * Return true if the printer firmware needs to be updated.
-         * TODO: where to find this data for different printer versions?
-         * @returns {boolean}
-         */
-        needsFirmwareUpgrade: function () {
-            if (this.firmware) {
-                if (this.type_id === '7FAF097F-DB2E-45DC-9395-A30210E789AA') {
-                    if (versionCompare(this.firmware, _kLatestFirmwareVersion) === -1) {
-                        return true;
-                    }
-                } else {
-                    // TODO: other printer types
-                }
-            }
-            return false;
         },
 
         /**
