b4acf481678e01cbff6ffd8521f4e36bc1c70863
Paul Breslin <paul.breslin@autodesk.com>
2015-04-23 19:47:41 -0400

    Fixes to the task polling. Add import test.

--- sdk/src/Tasks.js
+++ sdk/src/Tasks.js
@@ -3,46 +3,54 @@ var ADSKSpark = ADSKSpark || {};
 
 ADSKSpark.TaskWaiter = function( progressCallback )
 {
+    var Client = ADSKSpark.Client;
+    var _this = this;
     var _taskId, _interval;
 
-    function checkTaskResponse(taskResponse)
+    this._checkTaskResponse = function(taskResponse)
     {
-        if( taskResponse.body.status === 'done' )
+        var delayPromise;
+
+        console.log("Task status: ", taskResponse.status, "progress:", taskResponse.progress);
+
+        if( taskResponse.status === 'done' )
         {
-            var result = res.body.result;
+            var result = taskResponse.result;
             return result;
         }
-        if( taskResponse.body.status === 'error' )
+        if( taskResponse.status === 'error' )
         {
             return Promise.reject(taskResponse);
         }
         if( progressCallback )
-            progressCallback( taskResponse.body.progress );
+            progressCallback( taskResponse.progress );
 
-        return new Promise(function(resolve, reject) {
-            setTimeout(_this._pollTask, interval);
-        });
+        console.log("Delay task");
+
+        return new Promise(function(resolve) {
+            setTimeout(function() { resolve(); }, _interval);
+        }).then( _this._pollTask );
     }
 
 
     this._pollTask = function()
     {
+        console.log("Poll task");
         return Client.authorizedApiRequest('/print/tasks/' + _taskId)
-                .post().then(checkTaskResponse);
+                .get().then(_this._checkTaskResponse);
     }
 
 
     this.wait = function(opResult, interval) 
     {
-        var _this = this;
-
         interval = interval || 100;
         if( interval < 10 )
             interval = 10;
 
         _taskId = opResult.id; // or is it body.id ?
+        _interval = interval;
 
 
-        return this._pollTask();
+        return _this._pollTask();
     };
 };
