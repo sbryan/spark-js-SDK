de07ba5c89ffb88ad4ef4116a26a9244ad38bc38
Greg Rashkevitch <rashkevitch@gmail.com>
2015-05-09 08:46:18 +0300

    Make the config of the server clearer to understand

--- authentication_server/nodejs/server.js
+++ authentication_server/nodejs/server.js
@@ -2,7 +2,7 @@
 var env = process.env.ENV || 'local',
     port = process.env.PORT || 3000,
     API_SERVER = (env === 'prod' ? 'https://api.spark.autodesk.com/api/v1' : 'https://sandbox.spark.autodesk.com/api/v1');
-    config = require('./config.example.js'),
+    config = require('./config.js');
 
 //Make sure port is a number
 if (isNaN(port)){
