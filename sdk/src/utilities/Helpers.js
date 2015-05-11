1bbb2f43b13b69d0c75419bf2df07c33bc6d1f0a
Steven Watanabe <steven.watanabe@autodesk.com>
2015-05-11 14:28:23 -0400

    Add a utility method to convert some json to an encoded parameter string

--- sdk/src/utilities/Helpers.js
+++ sdk/src/utilities/Helpers.js
@@ -39,6 +39,7 @@ ADSKSpark.Helpers = function () {
 			}
 			return params;
 		},
+
 		/**
 		 * Extract params from URL
 		 * @returns {{}}
@@ -49,9 +50,44 @@ ADSKSpark.Helpers = function () {
 
 			return getParams;
 		},
+
+        /**
+         * Convert some json to an encoded parameter string.
+         * For example {k1: v1, k2: v2} -> 'k1=v1&k2=v2'.
+         * @param {Object} json.
+         * @param {function|string[]|string} [filter] Optional json key filter.
+         * @returns {!string}
+         */
+        jsonToParameters: function (json, filter) {
+            if (!json) {
+                return null;
+            }
+
+            var keys = Object.keys(json);
+            if (0 === keys) {
+                return null;
+            }
+
+            if (filter) {
+                if (typeof filter === 'function') {
+                    keys = keys.filter(filter);
+                } else if (Array.isArray(filter)) {
+                    keys = keys.filter(function (key) {
+                        return filter.indexOf(key) === -1;
+                    })
+                } else { // string
+                    keys = keys.filter(function (key) {
+                        return key !== filter;
+                    })
+                }
+            }
+
+            return keys.map(function (key) {
+                return encodeURIComponent(key) + '=' + encodeURIComponent(json[key]);
+            }).join('&');
+        }
 	};
 
 	return helpers;
-
 
 }();
