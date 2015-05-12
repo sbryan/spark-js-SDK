6d1fae8111f8bd6f59efbb8ac4b8f159de3ff29d
Greg Rashkevitch <rashkevitch@gmail.com>
2015-05-12 21:10:06 +0300

    Minor fixes to tests

--- sdk/test/drive/MembersSpec.js
+++ sdk/test/drive/MembersSpec.js
@@ -1,21 +1,26 @@
 describe('Members', function () {
 	'use strict';
 
-	var Members, Client, mockedAuthorizedRequest, fakeMember;
+	var Members, mockedAuthorizedRequest, fakeMember;
 
 
 	before(function () {
 		Members = ADSKSpark.Members;
-		Client = ADSKSpark.Client;
+		var Client = ADSKSpark.Client;
 		mockedAuthorizedRequest = sinon.stub(Client, 'authorizedApiRequest');
 	});
 
 	beforeEach(function () {
+
+		//this object is partial and contains many more fields
 		fakeMember = {
 			member: {
 				id: 10002,
 				first_name: 'John',
-				last_name: 'Doe'
+				last_name: 'Doe',
+				profile:{
+					avatar_path:'https://static.spark.autodesk.com/image.png'
+				}
 			}
 		};
 	});
@@ -41,6 +46,8 @@ describe('Members', function () {
 			expect(response.member).to.have.property('id', fakeMember.member.id);
 			expect(response.member).to.have.property('first_name', fakeMember.member.first_name);
 			expect(response.member).to.have.property('last_name', fakeMember.member.last_name);
+			expect(response.member).to.have.property('profile', fakeMember.member.profile);
+			expect(response.member).to.have.deep.property('profile.avatar_path', fakeMember.member.profile.avatar_path);
 		});
 
 	});
@@ -71,6 +78,7 @@ describe('Members', function () {
 			expect(response.member).to.have.property('id', fakeMember.member.id);
 			expect(response.member).to.have.property('first_name', fakeMember.member.first_name);
 			expect(response.member).to.have.property('last_name', fakeMember.member.last_name);
+			expect(response.member).to.have.deep.property('profile.avatar_path', fakeMember.member.profile.avatar_path);
 		});
 
 	});
