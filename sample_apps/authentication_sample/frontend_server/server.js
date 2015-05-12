3124d76b80ddf9bb3c566caf1cfb9554f1595028
Paul Breslin <paul.breslin@autodesk.com>
2015-05-12 11:00:18 -0400

    Correct route to sdk source.

--- sample_apps/authentication_sample/frontend_server/server.js
+++ sample_apps/authentication_sample/frontend_server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/../../../src')); // Make the SDK available from the server root
+app.use(express.static(__dirname + '/../../../sdk/src')); // Make the SDK available from the server root
 
 app.listen(8000);
 
