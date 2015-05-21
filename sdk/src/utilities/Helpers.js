82c75c267785bff02878304d803cc495e126d992
Greg Rashkevitch <rashkevitch@gmail.com>
2015-05-21 20:55:46 +0300

    Minor fixes - if should have curly brackets

--- sdk/src/utilities/Helpers.js
+++ sdk/src/utilities/Helpers.js
@@ -1,6 +1,7 @@
 var ADSKSpark = ADSKSpark || {};
 
 /**
+ * @class
  * Our utilities object
  */
 ADSKSpark.Helpers = function () {
@@ -9,7 +10,7 @@ ADSKSpark.Helpers = function () {
 	/**
 	 * Return the factory object
 	 */
-	var helpers =  {
+	var helpers = {
 
 		/**
 		 * Open window in the center of the screen
@@ -46,54 +47,54 @@ ADSKSpark.Helpers = function () {
 		 * Extract params from URL
 		 * @returns {{}}
 		 */
-		extractParamsFromURL: function(prmstr){
+		extractParamsFromURL: function (prmstr) {
 			prmstr = prmstr || window.location.search.substr(1);
 			var getParams = prmstr ? helpers.transformToAssocArray(prmstr) : [];
 
 			return getParams;
 		},
 
-        /**
-         * Convert some json to an encoded parameter string.
-         * For example {k1: v1, k2: v2} -> 'k1=v1&k2=v2'.
-         * @param {Object} json.
-         * @param {function|string[]|string} [filter] Optional json key filter.
-         * @returns {!string}
-         */
-        jsonToParameters: function (json, filter) {
-            if (!json) {
-                return null;
-            }
+		/**
+		 * Convert some json to an encoded parameter string.
+		 * For example {k1: v1, k2: v2} -> 'k1=v1&k2=v2'.
+		 * @param {Object} json.
+		 * @param {function|string[]|string} [filter] Optional json key filter.
+		 * @returns {!string}
+		 */
+		jsonToParameters: function (json, filter) {
+			if (!json) {
+				return null;
+			}
 
-            var keys = Object.keys(json);
-            if (0 === keys) {
-                return null;
-            }
+			var keys = Object.keys(json);
+			if (0 === keys) {
+				return null;
+			}
 
-            if (filter) {
-                if (typeof filter === 'function') {
-                    keys = keys.filter(filter);
-                } else if (Array.isArray(filter)) {
-                    keys = keys.filter(function (key) {
-                        return filter.indexOf(key) === -1;
-                    });
-                } else { // string
-                    keys = keys.filter(function (key) {
-                        return key !== filter;
-                    });
-                }
-            }
+			if (filter) {
+				if (typeof filter === 'function') {
+					keys = keys.filter(filter);
+				} else if (Array.isArray(filter)) {
+					keys = keys.filter(function (key) {
+						return filter.indexOf(key) === -1;
+					});
+				} else { // string
+					keys = keys.filter(function (key) {
+						return key !== filter;
+					});
+				}
+			}
 
-            return keys.map(function (key) {
-                return encodeURIComponent(key) + '=' + encodeURIComponent(json[key]);
-            }).join('&');
-        },
+			return keys.map(function (key) {
+				return encodeURIComponent(key) + '=' + encodeURIComponent(json[key]);
+			}).join('&');
+		},
 		/**
 		 * Checks whether some supplied ID that is supposed to be an integer is valid
 		 * @param itemId - Such as assetId or fileId or memberId
 		 * @returns {*|boolean}
 		 */
-		isValidId: function(itemId){
+		isValidId: function (itemId) {
 			return itemId && !isNaN(itemId);
 		},
 
@@ -102,16 +103,28 @@ ADSKSpark.Helpers = function () {
 		 * @param itemIds - Such as assetIds or fileIds or memberIds
 		 * @returns {*|boolean}
 		 */
-		isValidIds: function(itemIds){
-			// var postiveInt = /^\d+$;
-			if(!itemIds)                   return false;
+		isValidIds: function (itemIds) {
+
+			if (!itemIds) {
+				return false;
+			}
 			var arrayForm = itemIds.toString().split(',');
-			if(arrayForm.length === 0)      return false;
+			if (arrayForm.length === 0) {
+				return false;
+			}
 			for (var i = 0; i < arrayForm.length; i++) {
-				if (!arrayForm[i])                                    return false;
-				if (isNaN(arrayForm[i]))                              return false;
-				if (arrayForm[i] != parseInt(arrayForm[i], 10))       return false;
-				if (arrayForm[i] < 0)                                 return false;
+				if (!arrayForm[i]) {
+					return false;
+				}
+				if (isNaN(arrayForm[i])){
+					return false;
+				}
+				if (arrayForm[i] !== parseInt(arrayForm[i], 10)){
+					return false;
+				}
+				if (arrayForm[i] < 0){
+					return false;
+				}
 			}
 			return true;
 		}
